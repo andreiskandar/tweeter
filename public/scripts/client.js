35b351b16956e92c729571ac002991a324d520c5
Andre Iskandar <andre.m.iskandar@gmail.com>
2020-09-07 17:43:54 -0700

    added arrow-down

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -4,3 +4,11 @@
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
 
+$('#tweet-text').keyup((e) => {
+	const maxChar = 140;
+	const len = $();
+});
+
+$('#arrow-down').on('click', () => {
+	$('#tweet-text').focus();
+});
