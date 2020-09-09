8d55af06e5da47dfe261df6e1dd366937e5ac53e
Andre Iskandar <andre.m.iskandar@gmail.com>
2020-09-08 19:18:36 -0700

    imported momentjs

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -15,7 +15,22 @@ $(document).ready(function () {
 			content: {
 				text: 'If I have seen further it is by standing on the shoulders of giants',
 			},
-			created_at: 1461116232227,
+			// created_at: 1461116232227,
+			created_at: 1599616494199,
+		},
+	];
+
+	const secondTweet = [
+		{
+			user: {
+				name: 'Descartes',
+				avatars: 'https://i.imgur.com/nlhLi3I.png',
+				handle: '@rd',
+			},
+			content: {
+				text: 'Je pense , donc je suis',
+			},
+			created_at: 1599614494199,
 		},
 	];
 
@@ -23,6 +38,10 @@ $(document).ready(function () {
 		const { name, avatars, handle } = newTweet[0].user;
 		const message = newTweet[0].content.text;
 		const createdAt = newTweet[0].created_at;
+		console.log(Date.now());
+		const diff = moment(createdAt).fromNow();
+
+		// console.log('diff:', diff.getSeconds());
 
 		const tweetHTML = `
 			<article>
@@ -35,7 +54,7 @@ $(document).ready(function () {
 				</div>
 				<p class="tweet-text">${message}</p>
 				<div class="more-info">
-					<div id="created-at">${createdAt}</div>
+					<div id="created-at">${diff}</div>
 					<div class="icons">
 						<i class="fas fa-flag fa-xs"></i>
 						<i class="far fa-retweet fa-xs"></i>
@@ -49,6 +68,8 @@ $(document).ready(function () {
 	};
 
 	const $tweet = createTweetElement(newTweet);
+	const $secTweet = createTweetElement(secondTweet);
 	$('main').append($tweet);
+	$('main').append($secTweet);
 	// console.log($tweet);
 });
