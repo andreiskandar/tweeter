83c141a5fce14eb2c20a345d71b745aea0b90f74
Andre Iskandar <andre.m.iskandar@gmail.com>
2020-09-09 08:53:19 -0700

    tested arrow down to focus on textarea

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -5,7 +5,7 @@
  */
 
 $(document).ready(function () {
-	const newTweet = [
+	const data = [
 		{
 			user: {
 				name: 'Newton',
@@ -15,12 +15,8 @@ $(document).ready(function () {
 			content: {
 				text: 'If I have seen further it is by standing on the shoulders of giants',
 			},
-			// created_at: 1461116232227,
-			created_at: 1599616494199,
+			created_at: 1599616994199,
 		},
-	];
-
-	const secondTweet = [
 		{
 			user: {
 				name: 'Descartes',
@@ -35,9 +31,9 @@ $(document).ready(function () {
 	];
 
 	const createTweetElement = (newTweet) => {
-		const { name, avatars, handle } = newTweet[0].user;
-		const message = newTweet[0].content.text;
-		const createdAt = newTweet[0].created_at;
+		const { name, avatars, handle } = newTweet.user;
+		const message = newTweet.content.text;
+		const createdAt = newTweet.created_at;
 		const diff = moment(createdAt).fromNow();
 
 		// console.log('diff:', diff.getSeconds());
@@ -66,9 +62,12 @@ $(document).ready(function () {
 		return tweetHTML;
 	};
 
-	const $tweet = createTweetElement(newTweet);
-	const $secTweet = createTweetElement(secondTweet);
-	$('main').append($tweet);
-	$('main').append($secTweet);
-	// console.log($tweet);
+	const renderTweets = (tweets) => {
+		const render = tweets.map((tweet) => {
+			const newTweet = createTweetElement(tweet);
+			$('.container').append(newTweet);
+		});
+	};
+
+	renderTweets(data);
 });
