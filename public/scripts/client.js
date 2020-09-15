b69b7b0968ee153a6f8c7ff0988a1a862331c42c
Andre Iskandar <andre.m.iskandar@gmail.com>
2020-09-15 13:30:08 -0700

    convert tab to spaces indentation

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -5,69 +5,69 @@
  */
 
 $(document).ready(function () {
-	// Reset text area onloading the page
-	$('#tweet-text').val('');
+  // Reset text area onloading the page
+  $('#tweet-text').val('');
 
-	// constants
-	const ERROR_MESSAGE = {
-		too_long: 'Please try a shorter tweet',
-		empty: 'Please type a tweet',
-	};
+  // constants
+  const ERROR_MESSAGE = {
+    too_long: 'Please try a shorter tweet',
+    empty: 'Please type a tweet',
+  };
 
-	// Nav button to toggle tweet text box
-	$('.home').click(() => {
-		$('html,body').animate({ scrollTop: 0 }, 1000);
-		$('.new-tweet').toggle('slow');
-		$('#tweet-text').focus();
-	});
+  // Nav button to toggle tweet text box
+  $('.home').click(() => {
+    $('html,body').animate({ scrollTop: 0 }, 1000);
+    $('.new-tweet').toggle('slow');
+    $('#tweet-text').focus();
+  });
 
-	// Home button to navigate to tweet text box
-	$('.home-button').click(() => {
-		$('html,body').animate({ scrollTop: 0 }, 1000);
-		$('.new-tweet').show();
-		$('#tweet-text').focus();
-	});
+  // Home button to navigate to tweet text box
+  $('.home-button').click(() => {
+    $('html,body').animate({ scrollTop: 0 }, 1000);
+    $('.new-tweet').show();
+    $('#tweet-text').focus();
+  });
 
-	// Nav button will appear when window scroll is below input text area
-	$(window).scroll(function () {
-		const userInputTextYLocation = 100;
-		const scroll = window.scrollY;
-		if (scroll > userInputTextYLocation) {
-			$('.home-button').css('visibility', 'visible');
-		} else {
-			$('.home-button').css('visibility', 'hidden');
-		}
-	});
+  // Nav button will appear when window scroll is below input text area
+  $(window).scroll(function () {
+    const userInputTextYLocation = 100;
+    const scroll = window.scrollY;
+    if (scroll > userInputTextYLocation) {
+      $('.home-button').css('visibility', 'visible');
+    } else {
+      $('.home-button').css('visibility', 'hidden');
+    }
+  });
 
-	// Escape function to prevent malicious attacks
-	const escape = (str) => {
-		let div = document.createElement('div');
-		div.appendChild(document.createTextNode(str));
-		return div.innerHTML;
-	};
+  // Escape function to prevent malicious attacks
+  const escape = (str) => {
+    let div = document.createElement('div');
+    div.appendChild(document.createTextNode(str));
+    return div.innerHTML;
+  };
 
-	const validatingForm = () => {
-		const textInput = $('#tweet-text').val();
+  const validatingForm = () => {
+    const textInput = $('#tweet-text').val();
 
-		if (textInput.length > 140) {
-			return { status: false, message: ERROR_MESSAGE.too_long };
-		} else if (!textInput || textInput.length === 1) {
-			// textInput.length to send error if enter is pressed with no message
-			return {
-				status: false,
-				message: ERROR_MESSAGE.empty,
-			};
-		}
-		return { status: true };
-	};
+    if (textInput.length > 140) {
+      return { status: false, message: ERROR_MESSAGE.too_long };
+    } else if (!textInput || textInput.length === 1) {
+      // textInput.length to send error if enter is pressed with no message
+      return {
+        status: false,
+        message: ERROR_MESSAGE.empty,
+      };
+    }
+    return { status: true };
+  };
 
-	const createTweetElement = (newTweet) => {
-		const { name, avatars, handle } = newTweet.user;
-		const message = newTweet.content.text;
-		const createdAt = newTweet.created_at;
-		const diff = moment(createdAt).fromNow();
+  const createTweetElement = (newTweet) => {
+    const { name, avatars, handle } = newTweet.user;
+    const message = newTweet.content.text;
+    const createdAt = newTweet.created_at;
+    const diff = moment(createdAt).fromNow();
 
-		const tweetHTML = `
+    const tweetHTML = `
 <article>
 <div class="tweet-header">
   <div class="profile">
@@ -88,62 +88,62 @@ $(document).ready(function () {
 </article>
 `;
 
-		return tweetHTML;
-	};
+    return tweetHTML;
+  };
 
-	const renderTweets = (tweets) => {
-		$('#tweets-container').empty();
-		const render = tweets.map((tweet) => {
-			const newTweet = createTweetElement(tweet);
-			$('#tweets-container').prepend(newTweet);
-		});
-	};
+  const renderTweets = (tweets) => {
+    $('#tweets-container').empty();
+    const render = tweets.map((tweet) => {
+      const newTweet = createTweetElement(tweet);
+      $('#tweets-container').prepend(newTweet);
+    });
+  };
 
-	const renderErrorMessage = (message) => {
-		const errorDiv = $(
-			`<div class="error-message"><i class="fal fa-exclamation-triangle"></i>${message}<i class="fal fa-exclamation-triangle"></i></div>`
-		).hide();
-		$('section').append(errorDiv);
-		errorDiv.slideDown(900);
-	};
+  const renderErrorMessage = (message) => {
+    const errorDiv = $(
+      `<div class="error-message"><i class="fal fa-exclamation-triangle"></i>${message}<i class="fal fa-exclamation-triangle"></i></div>`
+    ).hide();
+    $('section').append(errorDiv);
+    errorDiv.slideDown(900);
+  };
 
-	const loadTweets = () => {
-		$.get('http://localhost:8080/tweets').then((fetchedData) => renderTweets(fetchedData));
-	};
+  const loadTweets = () => {
+    $.get('http://localhost:8080/tweets').then((fetchedData) => renderTweets(fetchedData));
+  };
 
-	// Load old tweets when webpage loaded first time
-	loadTweets();
+  // Load old tweets when webpage loaded first time
+  loadTweets();
 
-	$('.form').submit(function (e) {
-		e.preventDefault();
-		// remove error-message element if exists
-		$('.error-message').remove();
-		const formValidation = validatingForm();
+  $('.form').submit(function (e) {
+    e.preventDefault();
+    // remove error-message element if exists
+    $('.error-message').remove();
+    const formValidation = validatingForm();
 
-		// validate form and set error message
-		if (!formValidation.status) {
-			renderErrorMessage(formValidation.message);
-			return;
-		}
-		const serializedData = $(this).serialize();
-		$.post('http://localhost:8080/tweets', serializedData).then(() => {
-			loadTweets();
+    // validate form and set error message
+    if (!formValidation.status) {
+      renderErrorMessage(formValidation.message);
+      return;
+    }
+    const serializedData = $(this).serialize();
+    $.post('http://localhost:8080/tweets', serializedData).then(() => {
+      loadTweets();
 
-			// empty tweeter input area
-			$('#tweet-text').val('');
+      // empty tweeter input area
+      $('#tweet-text').val('');
 
-			// reset character count
-			$('#chars').text(140);
-		});
-	});
+      // reset character count
+      $('#chars').text(140);
+    });
+  });
 
-	// Allow user to submit tweet pressing 'enter' button
-	$('.form').keyup(function (e) {
-		e.preventDefault();
-		// 13 for enter
-		if (e.which === 13) {
-			$('.form').submit();
-			return;
-		}
-	});
+  // Allow user to submit tweet pressing 'enter' button
+  $('.form').keyup(function (e) {
+    e.preventDefault();
+    // 13 for enter
+    if (e.which === 13) {
+      $('.form').submit();
+      return;
+    }
+  });
 });
--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,16 +1,16 @@
 $(document).ready(() => {
-	const maxChar = 140;
+  const maxChar = 140;
 
-	//character count listener
-	$('#tweet-text').on('keyup', function() {
-		let length = $(this).val().length;
-		let output = this.nextElementSibling.children.counter.children;
-		if (length > 140) {
-			$('#chars').css('color', 'red');
-		} else {
-			$('#chars').css('color', 'inherit');
-		}
-		length = maxChar - length;
-		output[0].innerHTML = length;
-	});
+  //character count listener
+  $('#tweet-text').on('keyup', function () {
+    let length = $(this).val().length;
+    let output = this.nextElementSibling.children.counter.children;
+    if (length > 140) {
+      $('#chars').css('color', 'red');
+    } else {
+      $('#chars').css('color', 'inherit');
+    }
+    length = maxChar - length;
+    output[0].innerHTML = length;
+  });
 });
